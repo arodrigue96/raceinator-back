11c7b6b5175f8e6280966637eced9cf1f55caf41
Aniol Rodriguez <arodrigue96@gmail.com>
2024-12-01 12:10:11 +0100

    Create Team type

---
+++ src/team/types.ts
@@ -0,0 +1,11 @@
+export interface Team {
+  id: string;
+  name: string;
+  ridersNames: string[];
+  championshipTitles: number;
+  imageUrl: string;
+  altImageText: string;
+  description: string;
+  debutYear: number;
+  isOficialTeam: boolean;
+}
