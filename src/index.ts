19fee56ea547862e21b4f8147799515c96813f84
Aniol Rodriguez <arodrigue96@gmail.com>
2024-12-02 15:50:58 +0100

    Change .env name variable and package.json start script

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import connectToDatabase from "./database/connectToDatabase.js";
 import { app } from "./server/middlewares.js";
 
 const port = process.env.PORT;
-const databaseUrl = process.env.DATA_BASE;
+const databaseUrl = process.env.DATABASE_URL;
 
 if (!databaseUrl) {
   throw new Error(chalk.bgRedBright("MongoDB url does not exist"));
